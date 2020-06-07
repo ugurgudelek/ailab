4e1e6fade20833589603713ac2630ec30be0268e
Uğur Güdelek <ugurgudelek@gmail.com>
2020-06-07 05:59:24 +0300

    Updates

---
+++ precache-manifest.dddd5fbd07a4e3136e6d2fffbc548a71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a722ca0a95b9c172e5cefb52541b3ae6",
+    "url": "/ailab/index.html"
+  },
+  {
+    "revision": "2167c91b697d650bce17",
+    "url": "/ailab/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "073497d2859d3d0d6d9e",
+    "url": "/ailab/static/js/2.1d286780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ailab/static/js/2.1d286780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2167c91b697d650bce17",
+    "url": "/ailab/static/js/main.0b6d73f3.chunk.js"
+  },
+  {
+    "revision": "55c9edd5316f614bebcd",
+    "url": "/ailab/static/js/runtime-main.09ab5ebe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ailab/static/media/logo.5d5d9eef.svg"
+  }
+]);
