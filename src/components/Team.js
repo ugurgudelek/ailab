93bb6d4e7fe398cc562c931f97858f7657f343e0
Furkan Dolaşık <furkandol0640@gmail.com>
2020-06-19 03:59:44 +0300

    Avatar components added to Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../css/styles.css';
+import AvatarMale from './AvatarMale';
+import AvatarFemale from './AvatarFemale';
  
 const Team = () => {
     return (
@@ -10,30 +12,10 @@ const Team = () => {
                     <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                 </div>
                 <div className="row">
-                    <div className="col-lg-4">
-                        <div className="team-member">
-                            <img className="mx-auto rounded-circle" src={require("../assets/img/team/1.jpg")} alt="" />
-                            <h4>Kay Garland</h4>
-                            <p className="text-muted">Lead Designer</p>
-                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="team-member">
-                            <img className="mx-auto rounded-circle" src={require("../assets/img/team/2.jpg")} alt="" />
-                            <h4>Larry Parker</h4>
-                            <p className="text-muted">Lead Marketer</p>
-                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="team-member">
-                            <img className="mx-auto rounded-circle" src={require("../assets/img/team/3.jpg")} alt="" />
-                            <h4>Diana Petersen</h4>
-                            <p className="text-muted">Lead Developer</p>
-                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
-                        </div>
-                    </div>
+                    <AvatarMale/>
+                    <AvatarMale/>
+                    <AvatarFemale/>
+                    <AvatarFemale/>
                 </div>
                 <div className="row">
                     <div className="col-lg-8 mx-auto text-center"><p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
