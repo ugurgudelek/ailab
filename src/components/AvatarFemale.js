4a6d46d27047851a972cf9b26c0b93140d79d03c
Furkan Dolaşık <furkandol0640@gmail.com>
2020-06-19 03:59:08 +0300

    Avatar components added

---
+++ src/components/AvatarFemale.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/styles.css';
+
+const AvatarFemale = () => {
+    return (
+        <div className="col-lg-4">
+            <div className="team-member">
+                <img className="mx-auto rounded-circle" src={require("../assets/img/team/avatar_female.png")} alt="" />
+                <h4>Kay Garland</h4>
+                <p className="text-muted">Lead Designer</p>
+                <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
+            </div>
+        </div>
+    );
+}
+ 
+export default AvatarFemale;
