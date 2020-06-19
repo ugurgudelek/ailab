99b47c4b9b5f625362ce591295a13b8c06b132ea
Furkan Dolaşık <furkandol0640@gmail.com>
2020-06-19 03:21:19 +0300

    The button in home "Tell Me" linked to About page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../css/styles.css';
 
-const home = () => {
+const Home = () => {
     return (
        <div>
         <header className="masthead">
             <div className="container">
-                <div className="masthead-subheading">Welcome To Tobb ETU AI Lab!</div>
-                <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+                <div className="masthead-subheading black">Welcome To Tobb ETU AI Lab!</div>
+                <div className="masthead-heading text-uppercase black">It's Nice To Meet You</div>
+                <a className="btn btn-secondary btn-xl text-uppercase js-scroll-trigger" href="/About">Tell Me More</a>
             </div>
         </header>
        </div>
     );
 }
  
-export default home;
+export default Home;
