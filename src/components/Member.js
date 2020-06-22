ac785a7d0d9293084b8e6dcc98a6e88e31bb6fe6
Furkan Dolaşık <furkandol0640@gmail.com>
2020-06-22 15:02:43 +0300

    Member component added with props

---
+++ src/components/Member.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/styles.css';
+
+function Member (props) {
+    return (
+        <div className="col-lg-4">
+            <div className="team-member">
+                <a href="#!"><img className="mx-auto rounded-circle" src={require("../assets/img/team/avatar_male.png")} alt=""/></a>
+                <h4>{props.name}</h4>
+                <p className="text-dark">{props.title}</p>
+                <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
+            </div>
+        </div>
+    );
+}
+ 
+export default Member;
