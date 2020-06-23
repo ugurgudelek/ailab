f4a86c9c577078d2007ed8d661c95139e2d19012
Furkan Dolaşık <furkandol0640@gmail.com>
2020-06-24 01:40:02 +0300

    profs' pages added

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../css/styles.css';
+import { Link } from 'react-router-dom'
 
 function Member (props) {
     return (
         <div className="col-lg-3">
             <div className="team-member">
-                <a href={props.link}><img className="mx-auto rounded-circle" src={require("../assets/img/team/avatar_male.png")} alt=""/></a>
+                <Link to={props.link}><img className="mx-auto rounded-circle" src={require("../assets/img/team/avatar_male.png")} alt=""/></Link>
                 <h4>{props.name}</h4>
                 <p className="text-dark">{props.title}</p>
                 <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
---
+++ src/components/members/MuratOzbayoglu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MuratOzbayoglu = () => {
+    return (
+      <section className="page-section bg-light">
+         <h3 className="text-center">THIS IS MURAT OZBAYOGLU PAGE</h3>
+      </section>
+    );
+}
+ 
+export default MuratOzbayoglu;
