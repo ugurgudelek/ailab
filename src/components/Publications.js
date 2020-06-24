a2ad83d72e35897e0beaeb057822de07ac4bea17
Furkan Dolaşık <furkandol0640@gmail.com>
2020-06-24 13:46:53 +0300

    Paper components added to Publications

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../css/styles.css';
+import Paper from './Paper';
  
-const Publication = () => {
-    return (
-      <section className="page-section bg-light">
-         <h3 className="text-center">THIS IS PUBLICATIONS PAGE</h3>
-      </section>
-    );
+const Publications = () => {
+  return (
+    <section className="page-section bg-light">
+        <h3 className="text-center">THIS IS PUBLICATIONS PAGE</h3>
+          <Paper name="Paper1" date="mm.dd.yy"/>
+          <Paper name="Paper2" date="mm.dd.yy"/>
+          <Paper name="Paper3" date="mm.dd.yy"/>
+          <Paper name="Paper4" date="mm.dd.yy"/>
+
+    </section>
+  );
 }
  
-export default Publication;
+export default Publications;
