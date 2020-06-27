2d2d40284a18379e57e479571d7b7cb2aeab648f
Furkan Dolaşık <furkandol0640@gmail.com>
2020-06-27 18:18:07 +0300

    link props added

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../css/styles.css';
 import Paper from './Paper';
+import Pdf from './pdfs/sample.pdf';
  
 const Publications = () => {
   return (
     <section className="page-section bg-light">
         <h3 className="text-center">THIS IS PUBLICATIONS PAGE</h3>
-          <Paper name="Paper1" date="mm.dd.yy"/>
-          <Paper name="Paper2" date="mm.dd.yy"/>
-          <Paper name="Paper3" date="mm.dd.yy"/>
-          <Paper name="Paper4" date="mm.dd.yy"/>
+          <Paper name="Paper1" date="mm.dd.yy" link={Pdf}/>
+          <Paper name="Paper2" date="mm.dd.yy" link={Pdf}/>
+          <Paper name="Paper3" date="mm.dd.yy" link={Pdf}/>
+          <Paper name="Paper4" date="mm.dd.yy" link={Pdf}/>
 
     </section>
   );
