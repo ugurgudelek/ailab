0df42381f84fb1026a509486d3397e41260db7c3
Furkan Dolaşık <furkandol0640@gmail.com>
2020-07-01 11:31:26 +0300

    increased download icons size

--- src/components/Paper.js
+++ src/components/Paper.js
@@ -7,7 +7,7 @@ const Paper = (props) => {
         <div className="card bg-light">
             <header className="card-header"><Link className="text-dark btn-link" to="#!">{props.name}</Link></header>
             <div className="card-body">Writers-Writers-Writers-Writers-Writers-Writers-Writers-Writers
-                <a target="_blank" rel="noopener noreferrer" href={props.link}><i class="fas fa-download float-right text-dark btn-link"></i></a>
+                <a target="_blank" rel="noopener noreferrer" href={props.link}><i class="fas fa-download float-right text-dark btn-link display-5"></i></a>
             </div>
             <div className="card-body">{props.date}</div>
             <div className="card-body">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui adipisci reprehenderit, nihil sunt atque explicabo iure quaerat tenetur, nemo molestiae repudiandae consequatur quod voluptatem architecto, id facilis! Magnam, nihil in.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint adipisci cumque, numquam ipsam eius inventore illum non laudantium sed ab doloribus quos voluptas ipsa esse fuga quae, facere accusantium facilis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, officiis nemo. Magni quidem quod neque eius dolorum nobis. Minima exercitationem illum explicabo necessitatibus consectetur quibusdam sunt cum ratione eaque rem!</div>
