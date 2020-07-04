c51cd589416dfb4d416fe2855fbef1c2e25a754f
Furkan Dolaşık <furkandol0640@gmail.com>
2020-07-05 00:14:12 +0300

    Publications component created

---
+++ src/components/PublicationsHome.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import '../css/styles.css';
+
+const PublicationsHome = () => {
+    return (
+        <section className="page-section bg-light">
+                        <div class="container">
+                <h2>Publications</h2>
+                <table class="table">
+                    <tbody>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, omnis eaque? Minus, expedita? Quibusdam, aliquam.</td>
+                        <td>mm.dd.yy</td>
+                        <td>
+                            <a className="text-dark" href="#!">Read more
+                                <i class="fas fa-long-arrow-alt-right"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, omnis eaque? Minus, expedita? Quibusdam, aliquam.</td>
+                        <td>mm.dd.yy</td>
+                        <td>
+                            <a className="text-dark" href="#!">Read more
+                                <i class="fas fa-long-arrow-alt-right"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, omnis eaque? Minus, expedita? Quibusdam, aliquam.</td>
+                        <td>mm.dd.yy</td>
+                        <td>
+                            <a className="text-dark" href="#!">Read more
+                                <i class="fas fa-long-arrow-alt-right"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, omnis eaque? Minus, expedita? Quibusdam, aliquam.</td>
+                        <td>mm.dd.yy</td>
+                        <td>
+                            <a className="text-dark" href="#!">Read more
+                                <i class="fas fa-long-arrow-alt-right"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a className="text-dark" href="/Publications">See all the papers
+                                <i class="fas fa-angle-double-right ml-3"></i>
+                            </a>
+                        </td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    );
+}
+
+export default PublicationsHome;
