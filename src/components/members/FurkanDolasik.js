ad920bd839827ad8a7bdb4ad4162c94f9a861481
Furkan Dolaşık <furkandol0640@gmail.com>
2020-07-06 21:26:38 +0300

    Furkan Dolaşık page created

---
+++ src/components/members/FurkanDolasik.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FurkanDolasik = () => {
+    return (
+        <section className="page-section bg-light member">
+            <div className="bg-bis ">
+                <h3 className="p-6 text-left">Furkan Dolaşık <br/> <span>Student <br/><br/> furkandol0640@gmail.com</span></h3>
+            </div>
+        </section>
+    );
+}
+ 
+export default FurkanDolasik;
