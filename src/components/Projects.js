0b18a9bd1a59cfeb5e74460bb0e546e3695840cb
Furkan Dolaşık <furkandol0640@gmail.com>
2020-07-06 11:16:13 +0300

    Project component added to Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../css/styles.css';
 import Footer from './Footer';
+import Project from './Project';
  
 const Projects = () => {
     return (
         <section className="page-section bg-light mt-5">
-            <h3 className="text-center">THIS IS PROJECTS PAGE</h3>
+            <h3 className="text-center">PROJECTS</h3>
+            <Project/>
+            <Project/>
+            <Project/>
+            <Project/>
+            <Project/>
+            <Project/>
             <Footer/>
         </section>
     );
