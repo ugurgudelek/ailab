44f7f854ea307b8a8b07541cb6480ecc5009e6fe
Furkan Dolaşık <furkandol0640@gmail.com>
2020-07-06 23:10:56 +0300

    random image added (for person's image)

--- src/components/members/FurkanDolasik.js
+++ src/components/members/FurkanDolasik.js
@@ -3,8 +3,9 @@ import React from 'react';
 const FurkanDolasik = () => {
     return (
         <section className="page-section bg-light member">
-            <div className="bg-bis ">
-                <h3 className="p-6 text-left">Furkan Dolaşık <br/> <span>Student <br/><br/> furkandol0640@gmail.com</span></h3>
+            <div className="bg-bis">
+                <h3 className="p-6 text-left w-50 inline">Furkan Dolaşık <br/> <span>Student <br/><br/> furkandol0640@gmail.com</span></h3>
+                <img className="m-3" src="https://picsum.photos/250/350" alt=""/>
             </div>
         </section>
     );
