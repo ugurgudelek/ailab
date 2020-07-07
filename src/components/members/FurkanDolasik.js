a7a3c525df87e83cec2b3a492e5bedd4fac249ae
Furkan Dolaşık <furkandol0640@gmail.com>
2020-07-07 18:23:33 +0300

    new features added to Team member page

--- src/components/members/FurkanDolasik.js
+++ src/components/members/FurkanDolasik.js
@@ -3,9 +3,14 @@ import React from 'react';
 const FurkanDolasik = () => {
     return (
         <section className="page-section bg-light member">
+            <header className="team-header-2">FURKAN DOLAŞIK</header>
             <div className="bg-bis">
-                <h3 className="p-6 text-left w-50 inline">Furkan Dolaşık <br/> <span>Student <br/><br/> furkandol0640@gmail.com</span></h3>
-                <img className="m-3" src="https://picsum.photos/250/350" alt=""/>
+                <h3 className="p-6 text-left w-50 inline">Furkan Dolaşık<br/><span>Student</span> <hr/></h3>
+                <img className="m-3 w-25" src={require("../../assets/img/team/2.jpg")} alt=""/>
+            </div>
+            <div className="bg-light">
+                <p className="w-50 text-left inline mt-3 pl-3">furkandol0640@gmail.com <br/> if exist website <br/>  if exist tel number </p>
+                <p className="w-50 text-left inline mt-3 border-left pl-3 border-dark"> ABOUT!! Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, earum, reiciendis nostrum eaque facere provident eveniet porro sint, minus praesentium dolore deserunt beatae consequatur maiores esse ex voluptas neque laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quasi culpa sequi eveniet et enim esse delectus ipsam aspernatur labore? Odio sit minima explicabo perferendis itaque. Laborum debitis saepe at! Lorem ipsum dolor sit, amet consectetur adipisicing elit. At dicta repellat cupiditate a, maxime quibusdam veniam deleniti nostrum illo? Atque deserunt totam porro aspernatur asperiores voluptate. Distinctio provident ad atque.</p>
             </div>
         </section>
     );
