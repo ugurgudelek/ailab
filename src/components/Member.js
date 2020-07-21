413276ddcc4314fb877e7d06b289ce081a71f493
Furkan Dolaşık <furkandol0640@gmail.com>
2020-07-21 17:54:19 +0300

    Default Image added

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../css/styles.css';
 import { Link } from 'react-router-dom'
+import defaultImg from '../assets/img/team/avatar_male.png';
 
 function Member (props) {
     return (
         <div className="col-lg-3">
             <div className="team-member">
-                <Link to={props.link}><img className="mx-auto rounded-circle" src={require("../assets/img/team/avatar_male.png")} alt=""/></Link>
+                <Link to={props.link}><img className="mx-auto rounded-circle" src={props.img} alt=""/></Link>
                 <h4>{props.name}</h4>
                 <p className="text-dark">{props.title}</p>
                 <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-twitter"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-facebook-f"></i></a><a className="btn btn-dark btn-social mx-2" href="#!"><i className="fab fa-linkedin-in"></i></a>
@@ -17,6 +18,7 @@ function Member (props) {
 Member.defaultProps = {
     name : "No info for now",
     title : "No info for now",
-    link : "#!"
+    link : "#!",
+    img : defaultImg
 }
 export default Member;
