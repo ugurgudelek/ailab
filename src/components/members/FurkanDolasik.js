8a8ff02082601f70284026c7ffbe12907a8d5964
Furkan Dolaşık <furkandol0640@gmail.com>
2020-07-21 18:28:50 +0300

    image embeded to page

--- src/components/members/FurkanDolasik.js
+++ src/components/members/FurkanDolasik.js
@@ -9,10 +9,10 @@ const FurkanDolasik = () => {
                 <h3 className="p-6 text-left w-50 inline">Furkan Dolasik<br/>
                 <span>Student</span><hr/>
                 <Link to="/Team" className="text-dark"><i className=" pr-3" class="fas fa-long-arrow-alt-left"></i>Back to team</Link></h3>
-                <img className="m-3 w-25" src={require("../../assets/img/team/2.jpg")} alt=""/>
+                <img className="m-3 w-25" src={require("../../assets/img/team/furkandolasik.jpg")} alt=""/>
             </div>
             <div className="bg-light">
-                <p className="w-50 text-left inline mt-3 pl-5">furkandol0640@gmail.com <br/> if exist website <br/>  if exist tel number </p>
+                <p className="w-50 text-left inline mt-3 pl-5">furkandol0640@gmail.com <br/></p>
                 <p className="w-50 text-left inline mt-3 border-left pl-3 border-dark pr-3"> ABOUT!! Lorem nt eveniet porro sint, minus praesentium d ipulpa sequi eveniet et enim esse delectus ipsam aspernatur labore? Odio sit minima explicabo perferendis itaque. Laborum debitis saepe at! Lorem ipsum dolor sit, amet consectetur adipisicing elit. At dicta repellat cupiditate a, maxime quibusdam veniam deleniti nostrum illo? Atque deserunt totam porro aspernatur asperiores voluptate. Distinctio provident ad atque.</p>
             </div>
         </section>
