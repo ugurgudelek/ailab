707d0191573cbefbd87e3eb09822346da0756308
Furkan Dolaşık <furkandol0640@gmail.com>
2020-07-22 12:13:26 +0300

    contact background image added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import Iframe from 'react-iframe';
 
 const Contact = () => {
     return (
-        <section className="page-section bg-light mt-4 pb-0">
+        <section className="page-section bg-light mt-4 pb-0" id="team">
             <h3 className="text-center bg-dark-2 text-light pt-4 pb-4 mt-0 mb-5">CONTACT US</h3>
             <h4 className="text-center text-uppercase text-secondary">You can found us here</h4>
             <h4 className="text-center text-secondary large"><i class="fas fa-long-arrow-alt-down"></i></h4>
