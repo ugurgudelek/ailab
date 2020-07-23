0050067ef67173c727adeffac850614e4c298ae4
Furkan Dolaşık <furkandol0640@gmail.com>
2020-07-23 21:20:55 +0300

    projects page is more compatible for mobile

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,9 +7,9 @@ function Project (props) {
         <div className="col-12">
             <div className="project-member">
                 <Link to={props.link}><img src="https://picsum.photos/400/300" alt=""/></Link>
-                <h3 className="w-25">{props.name} Lorem ipsum dolor sit amet. </h3>
+                <h3 className="w-25-project">{props.name} Lorem ipsum dolor sit amet. </h3>
                 <h6><span>Posted on</span> {props.date}</h6>
-                <p className="text-dark w-50">{props.sum} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt delectus odio fugit incidunt eveniet quia sapiente. Doloremque vel temporibus perferendis...</p>
+                <p className="text-dark w-50-project">{props.sum} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt delectus odio fugit incidunt eveniet quia sapiente. Doloremque vel temporibus perferendis...</p>
                 <Link className="btn btn-info">Read More!</Link>
             </div>
         </div>
