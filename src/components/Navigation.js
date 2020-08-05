65b693749a536e6e4d1fa8b305df8f32520d0d31
Furkan Dolaşık <furkandol0640@gmail.com>
2020-08-05 10:38:12 +0300

    Home button always lit bug fixed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,7 +11,7 @@ const Navigation = () => {
                     <a className="navbar-brand js-scroll-trigger" target="_blank" rel="noopener noreferrer" href="https://www.etu.edu.tr/tr"><img src={require("../assets/img/tobb_etu_dikey_en.png")} alt="" /></a><button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i className="fas fa-bars ml-1"></i></button>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul className="navbar-nav text-uppercase ml-auto">
-                            <li className="nav-item"><NavLink className="nav-link js-scroll-trigger text-dark-2" to="/">Home</NavLink></li>
+                            <li className="nav-item"><NavLink exact className="nav-link js-scroll-trigger text-dark-2" to="/">Home</NavLink></li>
                             <li className="nav-item"><NavLink className="nav-link js-scroll-trigger text-dark-2" to="/About">About</NavLink></li>
                             <li className="nav-item"><NavLink className="nav-link js-scroll-trigger text-dark-2" to="/Team">Team</NavLink></li>
                             <li className="nav-item"><NavLink className="nav-link js-scroll-trigger text-dark-2" to="/Projects">Projects</NavLink></li>
