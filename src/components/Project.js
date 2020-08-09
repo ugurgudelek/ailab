db0c47e6abce45272bc8b2f3d6293af494292e98
Furkan Dolaşık <furkandol0640@gmail.com>
2020-08-09 16:45:08 +0300

    Project component edited

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,21 +4,29 @@ import { Link } from 'react-router-dom'
 
 function Project (props) {
     return (
-        <div className="col-12">
+        <div className="">
             <div className="project-member">
-                <Link to={props.link}><img src="https://picsum.photos/400/300" alt=""/></Link>
-                <h3 className="w-25-project">{props.name} Lorem ipsum dolor sit amet. </h3>
-                <h6><span>Posted on</span> {props.date}</h6>
-                <p className="text-dark w-50-project">{props.sum} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt delectus odio fugit incidunt eveniet quia sapiente. Doloremque vel temporibus perferendis...</p>
-                <Link className="btn btn-info">Read More!</Link>
+                <Link to={props.link}><img src={props.img} alt=""/></Link>
+                <h3 className="w-75-project project-name">{props.name}</h3>
+                <p className="w-60-project">{props.sum}</p>
+                <p className="w-60-project"><b>Funded by: </b>{props.fundedby}</p>
+                <p className="w-60-project"><b>Budget: </b>{props.budget}</p>
+                <p className="w-60-project"><b>Period: </b>{props.period}</p>
+                <p className="w-60-project"><b>Scope: </b>{props.scope}</p>
+                <p className="w-60-project"><b>Team: </b>{props.team}</p>
+                <p className="w-60-project"><b>Keywords: </b>{props.keywords}</p>
             </div>
         </div>
     );
 }
 Project.defaultProps = {
     name : "Project's name!!",
-    sum : "Summary!!",
+    fundedby : "lorem",
+    budget : "lorem",
+    period : "lorem",
+    scope : "lorem",
+    team : "lorem",
+    keywords : "lorem",
     date : "mm.dd.yy",
-    link : "#!"
 }
 export default Project;
