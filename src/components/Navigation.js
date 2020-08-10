95971be5c28a558489f9ae09ba0c69a95eee916d
Furkan Dolaşık <furkandol0640@gmail.com>
2020-08-10 10:08:24 +0300

    just arrangement

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../css/styles.css';
-
 import { NavLink } from 'react-router-dom';
  
 const Navigation = () => {
@@ -8,7 +7,8 @@ const Navigation = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-gray" id="mainNav">
                 <div className="container">
-                    <a className="navbar-brand js-scroll-trigger" target="_blank" rel="noopener noreferrer" href="https://www.etu.edu.tr/tr"><img src={require("../assets/img/tobb_etu_dikey_en.png")} alt="" /></a><button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i className="fas fa-bars ml-1"></i></button>
+                    <a className="navbar-brand js-scroll-trigger" target="_blank" rel="noopener noreferrer" href="https://www.etu.edu.tr/tr"><img src={require("../assets/img/tobb_etu_dikey_en.png")} alt="" /></a>
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="true" aria-label="Toggle navigation">Menu<i className="fas fa-bars ml-1"></i></button>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
                         <ul className="navbar-nav text-uppercase ml-auto">
                             <li className="nav-item"><NavLink exact className="nav-link js-scroll-trigger text-dark-2" to="/">Home</NavLink></li>
