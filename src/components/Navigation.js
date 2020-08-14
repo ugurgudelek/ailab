16665f5abf7e5aa145c99fae68963e6565bd1b2a
Furkan Dolaşık <furkandol0640@gmail.com>
2020-08-14 20:30:02 +0300

    button deleted / OwnDropdown added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/styles.css';
 import { NavLink } from 'react-router-dom';
+import OwnDropdown from './OwnDropdown';
  
 const Navigation = () => {
     return (
@@ -8,8 +9,7 @@ const Navigation = () => {
             <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-gray" id="mainNav">
                 <div className="container">
                     <a className="navbar-brand js-scroll-trigger" target="_blank" rel="noopener noreferrer" href="https://www.etu.edu.tr/tr"><img src={require("../assets/img/tobb_etu_dikey_en.png")} alt="" /></a>
-                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="true" aria-label="Toggle navigation">Menu<i className="fas fa-bars ml-1"></i></button>
-                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <div className="">
                         <ul className="navbar-nav text-uppercase ml-auto">
                             <li className="nav-item"><NavLink exact className="nav-link js-scroll-trigger text-dark-2" to="/">Home</NavLink></li>
                             <li className="nav-item"><NavLink className="nav-link js-scroll-trigger text-dark-2" to="/about">About</NavLink></li>
@@ -17,6 +17,7 @@ const Navigation = () => {
                             <li className="nav-item"><NavLink className="nav-link js-scroll-trigger text-dark-2" to="/projects">Projects</NavLink></li>
                             <li className="nav-item"><NavLink className="nav-link js-scroll-trigger text-dark-2" to="/publications">Publications</NavLink></li>
                             <li className="nav-item"><NavLink className="nav-link js-scroll-trigger text-dark-2" to="/contact">Contact</NavLink></li>
+                            <li><OwnDropdown/></li>
                         </ul>
                     </div>
                 </div>
