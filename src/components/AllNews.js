b2131cca8b738ce0142da1a70e3d741e811162bc
Furkan Dolaşık <furkandol0640@gmail.com>
2020-08-23 14:11:45 +0300

    allnews page added

---
+++ src/components/AllNews.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/styles.css';
+
+const AllNews = () => {
+    return (
+        <section className="page-section bg-dark mt-4 pb-0" id="team">
+            <div>
+                <h3 className="text-center bg-dark-2 text-light pt-4 pb-4 mt-0 mb-5">NEWS</h3>
+                <div className="container">
+                    
+                </div>
+            </div>
+        </section>
+    );
+}
+ 
+export default AllNews;
