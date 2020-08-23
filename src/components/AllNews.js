49f0bcbe679b65effe29e111ee1070677e8a7e22
Furkan Dolaşık <furkandol0640@gmail.com>
2020-08-23 14:31:12 +0300

    allnews fake content added

--- src/components/AllNews.js
+++ src/components/AllNews.js
@@ -7,7 +7,25 @@ const AllNews = () => {
             <div>
                 <h3 className="text-center bg-dark-2 text-light pt-4 pb-4 mt-0 mb-5">NEWS</h3>
                 <div className="container">
-                    
+                    <h4>News 1</h4>
+                    <hr/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus nemo hic, incidunt autem officiis excepturi obcaecati at facere, iusto harum dolor architecto eligendi et soluta eius? Perferendis harum asperiores vero in minima, necessitatibus odio nostrum facere assumenda ex eos, ratione iure earum repudiandae quis obcaecati neque dolorem vitae et? Quos rem natus libero ut vitae hic aut ipsum numquam facere repudiandae optio doloremque qui ipsam tenetur, tempore doloribus, provident sapiente perspiciatis molestias labore repellat id quibusdam. Accusantium ad deserunt nam ut obcaecati dolor dolorem beatae quidem, quae, repellendus, possimus sapiente saepe suscipit delectus ratione non quaerat quasi natus a.</p>
+                    <h4>News 2</h4>
+                    <hr/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus nemo hic, incidunt autem officiis excepturi obcaecati at facere, iusto harum dolor architecto eligendi et soluta eius? Perferendis harum asperiores vero in minima, necessitatibus odio nostrum facere assumenda ex eos, ratione iure earum repudiandae quis obcaecati neque dolorem vitae et? Quos rem natus libero ut vitae hic aut ipsum numquam facere repudiandae optio doloremque qui ipsam tenetur, tempore doloribus, provident sapiente perspiciatis molestias labore repellat id quibusdam. Accusantium ad deserunt nam ut obcaecati dolor dolorem beatae quidem, quae, repellendus, possimus sapiente saepe suscipit delectus ratione non quaerat quasi natus a.</p>
+                    <h4>News 3</h4>
+                    <hr/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus nemo hic, incidunt autem officiis excepturi obcaecati at facere, iusto harum dolor architecto eligendi et soluta eius? Perferendis harum asperiores vero in minima, necessitatibus odio nostrum facere assumenda ex eos, ratione iure earum repudiandae quis obcaecati neque dolorem vitae et? Quos rem natus libero ut vitae hic aut ipsum numquam facere repudiandae optio doloremque qui ipsam tenetur, tempore doloribus, provident sapiente perspiciatis molestias labore repellat id quibusdam. Accusantium ad deserunt nam ut obcaecati dolor dolorem beatae quidem, quae, repellendus, possimus sapiente saepe suscipit delectus ratione non quaerat quasi natus a.</p>
+                    <h4>News 4</h4>
+                    <hr/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus nemo hic, incidunt autem officiis excepturi obcaecati at facere, iusto harum dolor architecto eligendi et soluta eius? Perferendis harum asperiores vero in minima, necessitatibus odio nostrum facere assumenda ex eos, ratione iure earum repudiandae quis obcaecati neque dolorem vitae et? Quos rem natus libero ut vitae hic aut ipsum numquam facere repudiandae optio doloremque qui ipsam tenetur, tempore doloribus, provident sapiente perspiciatis molestias labore repellat id quibusdam. Accusantium ad deserunt nam ut obcaecati dolor dolorem beatae quidem, quae, repellendus, possimus sapiente saepe suscipit delectus ratione non quaerat quasi natus a.</p>
+                    <h4>News 5</h4>
+                    <hr/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus nemo hic, incidunt autem officiis excepturi obcaecati at facere, iusto harum dolor architecto eligendi et soluta eius? Perferendis harum asperiores vero in minima, necessitatibus odio nostrum facere assumenda ex eos, ratione iure earum repudiandae quis obcaecati neque dolorem vitae et? Quos rem natus libero ut vitae hic aut ipsum numquam facere repudiandae optio doloremque qui ipsam tenetur, tempore doloribus, provident sapiente perspiciatis molestias labore repellat id quibusdam. Accusantium ad deserunt nam ut obcaecati dolor dolorem beatae quidem, quae, repellendus, possimus sapiente saepe suscipit delectus ratione non quaerat quasi natus a.</p>
+                    <h4>News 6</h4>
+                    <hr/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam natus nemo hic, incidunt autem officiis excepturi obcaecati at facere, iusto harum dolor architecto eligendi et soluta eius? Perferendis harum asperiores vero in minima, necessitatibus odio nostrum facere assumenda ex eos, ratione iure earum repudiandae quis obcaecati neque dolorem vitae et? Quos rem natus libero ut vitae hic aut ipsum numquam facere repudiandae optio doloremque qui ipsam tenetur, tempore doloribus, provident sapiente perspiciatis molestias labore repellat id quibusdam. Accusantium ad deserunt nam ut obcaecati dolor dolorem beatae quidem, quae, repellendus, possimus sapiente saepe suscipit delectus ratione non quaerat quasi natus a.</p>
+                
                 </div>
             </div>
         </section>
